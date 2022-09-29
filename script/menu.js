90c0e6becb6b29d46f8f098d534f8272ee466538
water-day <113660748+water-day@users.noreply.github.com>
2022-09-29 16:59:31 +0900

    Delete script directory

--- script/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.nav> li').mouseover(function(){
-        $(this).children('.sub').stop().slideDown();
-    });
-    $('.nav> li').mouseout(function(){
-        $(this).children('.sub').stop().slideUp();
-    });
-});
